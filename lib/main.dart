c4c286a92003e7245ec86e36566448a42b40e975
Saroj Kaashyap Pokkula <sarojk@incentsoft.com>
2023-12-30 22:24:02 -0700

    Adding firebase auth and rest of the post calls

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:flutter_memory_game/providers/user_data.dart';
 import 'package:flutter_memory_game/views/login_screen.dart';
 import 'package:flutter_memory_game/views/start_game_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ChangeNotifierProvider(
       create: (context) => UserData(),
--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_memory_game/providers/user_data.dart';
 import 'package:flutter_memory_game/services/auth.dart';
@@ -15,16 +16,84 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passController = TextEditingController();
+  bool _isLoading = false;
 
-  Future<void> _handleLoginClick() async {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  bool _validateEmail(String email) {
+    final RegExp emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    return emailRegex.hasMatch(email);
+  }
+
+  Future<void> loginButtonHandler() async {
+    if (_isLoading) return;
+    if (_emailController.text.isNotEmpty && _passController.text.isNotEmpty) {
+      try {
+        if (!_validateEmail(_emailController.text)) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+                content: Text('Alert: Please enter a valid email address')),
+          );
+          return;
+        }
+        setState(() {
+          _isLoading = true;
+        });
+        final credentials =
+            await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _emailController.text,
+          password: _passController.text,
+        );
+        await handleGetUserData();
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found' || e.code == 'wrong-password') {
+          // Handle invalid email or password
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Invalid email or password'),
+            ),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('An error occurred. Please try again.'),
+            ),
+          );
+        }
+      } catch (e) {
+        // Handle other exceptions
+        print('Unexpected error: $e');
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('An unexpected error occurred. Please try again.'),
+          ),
+        );
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Alert: Please enter the required fields')),
+      );
+    }
+  }
+
+  Future<void> handleGetUserData() async {
     final username = _emailController.text;
     final password = _passController.text;
 
     try {
       final response = await loginUser(username, password);
       // Navigate to next screen or show success message
+      setState(() {
+        _isLoading = false;
+      });
       Provider.of<UserData>(context, listen: false).setUserData(response!);
-
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
@@ -165,19 +234,25 @@ class _LoginScreenState extends State<LoginScreen> {
                         width: double.infinity,
                         height: 56,
                         child: ElevatedButton(
-                          onPressed: _handleLoginClick,
+                          onPressed: loginButtonHandler,
                           style: ElevatedButton.styleFrom(
                             backgroundColor: const Color(0xFF9F7BFF),
                           ),
-                          child: const Text(
-                            'Login',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
+                          child: _isLoading
+                              ? const SizedBox(
+                                  height: 25,
+                                  width: 25,
+                                  child: CircularProgressIndicator(),
+                                )
+                              : const Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
                         ),
                       ),
                     ),
